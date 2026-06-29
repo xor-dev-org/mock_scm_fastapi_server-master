6e8ee71b7f31315e6faee06d1d91e16295a127e2
rnrathod <ritesh.rathod@xoriant.com>
2026-06-29 15:13:01 +0530

    Backend: ACS changes removed token and added api for all chats

--- app/integrations/chat_service/database/db.py
+++ app/integrations/chat_service/database/db.py
@@ -1,6 +1,8 @@
 import os
+from app.utils.postgres_db import find_many as pg_find_many
 from app.utils.postgres_db import find_one as pg_find_one
 from app.utils.postgres_db import insert_one as pg_insert_one
+from app.utils.postgres_db import update_one as pg_update_one
 
 CHAT_COLLECTION_NAME = os.getenv("CHAT_COLLECTION_NAME", "acs_chat_collection")
 
@@ -9,8 +11,14 @@ class PostgresChatCollection:
     def find_one(self, filter_value):
         return pg_find_one(CHAT_COLLECTION_NAME, filter_value)
 
+    def find_many(self, filter_value=None):
+        return pg_find_many(CHAT_COLLECTION_NAME, filter_value)
+
     def insert_one(self, document):
         return pg_insert_one(CHAT_COLLECTION_NAME, document)
+
+    def update_one(self, filter_value, update_value):
+        return pg_update_one(CHAT_COLLECTION_NAME, filter_value, update_value)
 
 
 chat_collection = PostgresChatCollection()
